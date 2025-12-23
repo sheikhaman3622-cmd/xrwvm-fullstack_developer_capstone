69be155ffc25aafcf3f672af493442b821183def
Sheikh Aman <sheikhaman3622@gmail.com>
2025-12-23 03:43:51 -0500

    Added Contact Us page with navigation and contact info

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),  # <- ADD THIS
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),  # <- ADD THIS
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
